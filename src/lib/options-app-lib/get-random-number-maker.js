93ba1dfc19bcbfa2f0f11719e4eabf891e873049
Alex Terliuk <alex.terliuk@gmail.com>
2020-09-12 23:06:18 +0300

    feat: add makeRandomNumber

---
+++ src/lib/options-app-lib/get-random-number-maker.js
@@ -0,0 +1,35 @@
+/**
+ * Provider of makeRandomNumber with bound context.
+ *   @param {object} valueChecker
+ *   @param {object} optionsApp - options defined in app
+ */
+function getRandomNumberMaker(valueChecker, optionsApp) {
+  const _v = valueChecker;
+  const _o = optionsApp;
+
+  return makeRandomNumber;
+
+  /**
+   * option.data:
+   *     {numbers} - min, max, digitsAfterFloatingPoint
+   */
+  function makeRandomNumber(option) {
+    const od = option.data;
+    const opt = {
+      min: _v.isNumber(od.min) && !_v.isInfinity(od.min) && !_v.isNaN(od.min) && od.min,
+      max: _v.isNumber(od.max) && !_v.isInfinity(od.max) && !_v.isNaN(od.max) && od.max,
+      digitsAfterFloatingPoint: _v.isNumber(od.digitsAfterFloatingPoint)
+                            && !_v.isInfinity(od.digitsAfterFloatingPoint)
+                            && !_v.isNaN(od.digitsAfterFloatingPoint)
+                            && od.digitsAfterFloatingPoint,
+    };
+
+    if (opt.min || opt.max) {
+      return +((_o.$getRandom.number(opt.min, opt.max)).toFixed(opt.digitsAfterFloatingPoint));
+    }
+
+    return 0;
+  }
+}
+
+module.exports = getRandomNumberMaker;
