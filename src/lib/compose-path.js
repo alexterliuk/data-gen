7634963e5e1a6830ee9326e8bf8a3435e02f0d29
Alex Terliuk <alex.terliuk@gmail.com>
2020-09-12 13:58:30 +0300

    feat: add composePath

---
+++ src/lib/compose-path.js
@@ -0,0 +1,25 @@
+/**
+ * Compose path from path segments.
+ *   @param {array} pathSegments - collection with strings and/or numbers
+ *   @param {object} options
+ */
+function composePath(pathSegments, options) {
+  const make = {
+    v1: (path, segment) => typeof segment === 'number' ? `[${segment}]` : !path.length ? `${segment}` : `.${segment}`,
+    v2: (path, segment) => typeof segment === 'number' ? `['${segment}']` : !path.length ? `${segment}` : `.${segment}`,
+    v3: (path, segment) => typeof segment === 'number' ? `["${segment}"]` : !path.length ? `${segment}` : `.${segment}`,
+    v4: (path, segment) => `['${segment}']`,
+    v5: (path, segment) => `["${segment}"]`,
+  };
+
+  //const syntax = ((options || {}).all || 'nooptions').pathSyntax;
+  const syntax = (options || {}).pathSyntax;
+  const syntaxType = !syntax ? 'v1'
+                             : syntax.singleQuotes && syntax.squareBraced ? 'v4'
+                             : syntax.singleQuotes ? 'v2'
+                             : syntax.doubleQuotes && syntax.squareBraced ? 'v5'
+                             : syntax.doubleQuotes ? 'v3'
+                             : 'v1';
+
+  return pathSegments.reduce((acc, segm) => acc += make[syntaxType](acc, segm), '');
+}
