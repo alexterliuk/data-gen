efa363b29294ab31075220e489cd9043dff99125
Alex Terliuk <alex.terliuk@gmail.com>
2020-09-15 22:29:31 +0300

    chore: improve readability in testMakeRandomNumber

--- src/test/test-built-in-lib/get-test-make-random-number.js
+++ src/test/test-built-in-lib/get-test-make-random-number.js
@@ -24,33 +24,82 @@ function getTestMakeRandomNumber() {
 
     params[0] = { msg1: 'min - 0, max - 1.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', 0, 'max', 1).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', 0, 'max', 1)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[1] = { msg1: 'min - 1, max - 1.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', 1, 'max', 1).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', 1, 'max', 1)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[2] = { msg1: 'min - -17, max - 0.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', -17, 'max', 0).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', -17, 'max', 0)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[3] = { msg1: 'min - 10, max - 0.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', 10, 'max', 0).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', 10, 'max', 0)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[4] = { msg1: 'min - NaN, max - 1.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', NaN, 'max', 1).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', NaN, 'max', 1)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[5] = { msg1: 'min - undefined, max - Infinity.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 30, byPath: [opt(getOption()).assign(false, 'min', undefined, 'max', Infinity).obtain()] }] };
+      vals: [{
+        quantity: 30,
+        byPath: [
+          opt(getOption()).assign(false, 'min', undefined, 'max', Infinity)
+                          .obtain()
+        ],
+      }],
+    };
 
     params[6] = { msg1: 'min - -1000, max - 1000, digitsAfterFloatingPoint - 3.',
       msg2: 'Some created numbers may have 2 or less digits after floating point. ' +
             'It happens when created number e.g. 50.0001, which cannot be shown as 50.000 - it\'s just 50. Same for 70.130 etc.',
       call: { args: [{ someNumber: 0 }, '_currVal_'] },
-      vals: [{ quantity: 100, byPath: [opt(getOption()).assign(false, 'min', -1000, 'max', 1000, 'digitsAfterFloatingPoint', 3).obtain()] }] };
+      vals: [{
+        quantity: 100,
+        byPath: [
+          opt(getOption()).assign(false, 'min', -1000, 'max', 1000, 'digitsAfterFloatingPoint', 3)
+                          .obtain()
+        ],
+      }],
+    };
 
 
     return logBuiltIn(count, params, 'testMakeRandomNumber');
